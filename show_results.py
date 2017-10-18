3cfa84ce13d4f908075d111f391b9c68197a1b80
Peyman Mortazavi <peyman.mortazavi@infoscoutinc.com>
2017-10-18 22:28:02 +0000

    more specific output

--- show_results.py
+++ show_results.py
@@ -17,7 +17,7 @@ with open(os.environ.get('RAW_FILE'), 'rb') as reader:
             continue
         parts = line.split(',')
         correct = parts[1]
-        prediction = parts[2]
+        prediction = parts[2].replace(' ', '_')
         confidence = parts[3]
         count_map[correct] += 1
         is_certain = float(confidence) >= threshold
@@ -33,17 +33,24 @@ with open(os.environ.get('RAW_FILE'), 'rb') as reader:
             if prediction != correct and is_prediction:
                 false_positives.add(line)
 
+    total_recall = 0
+    total_precision = 0
+    total_count = 0
     for label in labels:
         if label == 'none' or not label:
             continue
         if count_map[label] == 0:
             print('no tests were given for {}'.format(label))
             continue
+        total_count += count_map[label]
+        total_recall += recall_map[label]
+        total_precision += precision_map[label]
         recall = recall_map[label] / float(count_map[label]) * 100.0
         try:
             precision = precision_map[label] / float(recall_map[label]) * 100.0
         except:
             precision = 0.00
-        print('{} - Recall: {:.2f} ; Precision: {:.2f}'.format(label, recall, precision))
+        print('{} - Recall: {:.2f} ; Precision: {:.2f} ; Total Count: {}'.format(label, recall, precision, count_map[label]))
 
+    print('\nTotal Recall: {} ; Total Precision: {}'.format(total_recall/float(total_count) * 100.0, total_precision/float(total_recall) * 100.0))
     print('\nFalse Positives:\n{}\n'.format('\n'.join(false_positives)))
